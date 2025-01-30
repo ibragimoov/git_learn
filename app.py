860fa07365671be083a4aa00d9f89ad62ec51f00
Ibragimov Eldar <1ibragimov.e.i20@gmail.com>
2025-01-30 19:21:14 +0300

    добавили новые методы в класс и инкапсулировали поле name

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 class User:
-    name = ''
+    _name = ''
     age = ''
     password = ''
     email = ''
+    phone = ''
+    address = ''
+
+    def set_name(self, name):
+        self._name = name
 
     def get_name(self):
-        return self.name
+        return self._name
 
     def get_phone(self):
         return self.email
 
-
 user1 = User()
-user1.name = 'Bob'
-user1.email = 'bob@gmail.com'
+user1.set_name('Боб')
 print(user1.get_name())
